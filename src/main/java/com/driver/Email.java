3fe61e1adc6330cb0c370b44bbb555b5f84472d6
HarryPotter0107 <bhanuupadhyay87@gmail.com>
2023-11-04 23:50:24 +0530

    final commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+import java.util.regex.Pattern;
+
 public class Email {
 
     private String emailId;
@@ -25,5 +27,44 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        if(oldPassword.equals(this.password)){
+            if(isValidPassword(newPassword)){
+                this.password = newPassword;
+                //System.out.println("Password changed successfully.");
+            }
+//            else{
+//                System.out.println("The new Password is not valid.");
+//            }
+//        }else{
+//            System.out.println("The provided password doesn't match with old password");
+        }
+    }
+
+    private Boolean isValidPassword(String newPassword) {
+        Boolean capitalLetter = false;
+        Boolean smallLetter = false;
+        Boolean digit = false;
+        Boolean specialChar = false;
+
+        if(newPassword.length() < 8){
+            return false;
+        }
+        for (int i = 0; i < newPassword.length(); i++) {
+            char ch = newPassword.charAt(i);
+            if(ch >= 'A' && ch <= 'Z'){
+                capitalLetter = true;
+            }else if(ch >= 'a' && ch <= 'z'){
+                smallLetter = true;
+            }else if(ch >= '0' && ch <= '9'){
+                digit = true;
+            }else{
+                specialChar = true;
+            }
+        }
+        if(specialChar && smallLetter && capitalLetter && digit){
+            return true;
+        }
+        return false;
     }
 }
